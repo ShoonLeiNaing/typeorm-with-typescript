ee611a25d64d9c82a18e8be542d861c344ad4f13
ShoonLeiNaing <pandapanda254.pp@gmail.com>
2022-02-12 12:30:14 +0630

    db setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import { createConnection } from "typeorm";
+
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "shoonlei",
+      password: undefined,
+      database: "typeorm",
+    });
+    console.log("Database connect");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Unable to connect to database");
+  }
+};
+main();
